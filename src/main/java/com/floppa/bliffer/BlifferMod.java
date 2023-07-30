c9d03010bceaed46d0ea2537dc7a9ae9ca339166
Floppa God <93928325+FloppaG@users.noreply.github.com>
2023-07-29 23:52:21 -0700

    Update BlifferMod.java

--- src/main/java/com/floppa/bliffer/BlifferMod.java
+++ src/main/java/com/floppa/bliffer/BlifferMod.java
@@ -18,6 +18,6 @@ public class BlifferMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Loaded Bliffer Java");
+		LOGGER.info("Starting Bliffer Java");
 	}
 }
