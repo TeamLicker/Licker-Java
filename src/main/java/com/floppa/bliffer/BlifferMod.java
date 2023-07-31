3f3852e91f255759af6c10a47c3e9d3e822f56ef
FloppaG <floppagod46@gmail.com>
2023-07-30 17:08:03 -0700

    Brain hurt

--- src/main/java/com/floppa/bliffer/BlifferMod.java
+++ src/main/java/com/floppa/bliffer/BlifferMod.java
@@ -1,5 +1,7 @@
 package com.floppa.bliffer;
 
+import com.floppa.bliffer.item.ModItemGroups;
+import com.floppa.bliffer.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -14,10 +16,8 @@ public class BlifferMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		ModItemGroups.registerItemGroups();
 
-		LOGGER.info("Loaded Bliffer Java");
+		ModItems.registerModItems();
 	}
 }
