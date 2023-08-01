48261a90a3ec329bb9d56db06790a8c459017a03
FloppaG <floppagod46@gmail.com>
2023-07-31 17:03:23 -0700

    Added cape (s)

---
+++ src/main/java/com/floppa/bliffer/BlifferClient.java
@@ -0,0 +1,17 @@
+package com.floppa.bliffer;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class BlifferClient implements ClientModInitializer {
+    // Stores users who have capes
+    public static final String[][] capes = new String[][]{
+            new String[]{"FloppaGodJava", "floppa"},
+    };
+
+    public void onInitializeClient() {
+
+    }
+}
